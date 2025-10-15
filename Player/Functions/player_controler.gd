793235afe554b23d25838404b95c8ae38e2b122b
Blaine <messerblaine23@gmail.com>
2025-10-15 14:41:50 -0400

    Detect Purple Boxes

--- Player/Functions/player_controler.gd
+++ Player/Functions/player_controler.gd
@@ -1,10 +1,14 @@
 extends CharacterBody3D
 
+@export var Dev_Print: bool = false
+
 ## This is a Description
 @export var Base_Speed : float = 3.0
 @export var Sprint_Speed : float = 5.0
 @export var Movement_Acceleration : float = 4.0
 @export var Movement_Deceleration : float = 8.0
+
+
 
 @export var jump_height : float = 1.25
 @export var jump_time_to_peak : float = 0.4
@@ -25,14 +29,27 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 
+#	if is_on_wall():
+#		for i in range(get_slide_collision_count()):
+#			var collision = get_slide_collision(i)
+#			var collider = collision.get_collider()
+#			
+#			if collider == null:
+#				continue
+#
+#			# If the collider is the StaticBody3D inside the Purple Dev Box
+#			if collider.get_parent() and collider.get_parent().name == "Purple Dev Box":
+#				print("Touching the Purple Dev Box!")
+
+
+
 #Fix Stopping In Mid Air add Glide movement with no movement deceleration in air
 
 func Movement_Logic(delta: float) -> void:
 	Movement_Input = Input.get_vector("Left","Right","Forward","Backward").rotated(-Player_Camera.global_rotation.y)
 	var Horizontal_Movement : Vector2 = Vector2(velocity.x, velocity.z)
 	var Is_Sprinting: bool = Input.is_action_pressed("Sprint")
-
-
+	
 
 	if Movement_Input != Vector2.ZERO:
 		var Max_Speed : float = Sprint_Speed if Is_Sprinting else Base_Speed
@@ -61,6 +78,29 @@ func Movement_Logic(delta: float) -> void:
 		Horizontal_Movement = Horizontal_Movement.move_toward(Vector2.ZERO, Base_Speed * Movement_Deceleration * delta)
 		velocity.x = Horizontal_Movement.x
 		velocity.z = Horizontal_Movement.y
+
+
+	if is_on_wall() and Dev_Print == true:
+		var tree_node: Node = get_node("../Mesh/ClimbeableTree")
+
+		for i : int in range(get_slide_collision_count()):
+			var collision: KinematicCollision3D = get_slide_collision(i)
+			var collider: Object = collision.get_collider()
+			if collider == null:
+				continue
+
+			var parent_node: Node = collider.get_parent()
+			if parent_node == null:
+				continue
+
+			# Walk up the tree to see if we eventually reach ClimbeableTree
+			var current: Node = parent_node
+			while current:
+				if current == tree_node:
+					print("Touching ClimbeableTree child:", parent_node.name)
+					break
+				current = current.get_parent()
+
 
 func Jump_Logic(delta: float) -> void:
 	if is_on_floor():
